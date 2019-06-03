e811cbd84b96ace6587f030dcc4b514039d7ea7b
joaovictorferro <39158742+joaovictorferro@users.noreply.github.com>
2019-06-03 00:31:09 -0300

    Add files via upload

--- folhaDePagamento/src/programan/Main.java
+++ folhaDePagamento/src/programan/Main.java
@@ -10,11 +10,10 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		int i;
 		int diaMes = 1;
-		int mes = 1;
+		int mes = 0;
 		int id = 0;
-		int semanaQ = 0;
+		int semanaQ = 1;
 		Scanner sc = new Scanner(System.in);
-		// String buffer;
 
 		String[][] empregado = new String[100][15];
 
@@ -49,29 +48,30 @@ public class Main {
 
 			if (i == 1) {
 
-				for (j = 0; j < 100; j++) {
+				for (j = 0; j < 50; j++) {
 					if (idLivres[j] == 0) {
 						id = j;
 					}
 				}
 
-				System.out.println("Digite o nome do empregado:(somente o primeiro nome)");
+				System.out.println("Digite o nome do empregado (Somente o primeiro nome):");
 				empregado[id][0] = sc.next();
-				System.out.println("Digite o endereço do empregado:");
+				System.out.println("Digite o endereço do empregado (escreva tudo junto):");
 				empregado[id][1] = sc.next();
 				System.out.println("Digite se o empregado é Horista ou Assalariado:");
 				empregado[id][2] = sc.next();
-
-				// System.out.println(empregado[id][2]);
+				
 				if (empregado[id][2].equals("Horista")) {
-					System.out.println("Digite o valor da hora trabalhada do empregado:");
+					System.out.println("Digite o valor da hora trabalhada do empregado (ex: 200.00):");
 					empregado[id][3] = sc.next();
 					empregado[id][4] = "Nao";
 					empregado[id][5] = "0";
-					empregado[id][6] = "sexta";
+					empregado[id][8] = "sexta";
 					empregado[id][7] = "1";
+					empregado[id][14] = "0.0";
+					
 				} else if (empregado[id][2].equals("Assalariado")) {
-					System.out.println("Digite o salário do empregado:");
+					System.out.println("Digite o salário do empregado (ex: 200.00):");
 					empregado[id][14] = sc.next();
 					System.out.println("O empregado é comissionado?(Sim/Nao)");
 					empregado[id][4] = sc.next();
@@ -79,10 +79,11 @@ public class Main {
 					if (empregado[id][4].equals("Sim")) {
 						System.out.println("Digite o valor da comissão em porcentagem:");
 						empregado[id][5] = sc.next();
-						empregado[id][6] = "sexta";
-						empregado[id][7] = "1";
+						empregado[id][8] = "sexta";
+						empregado[id][7] = "2";
+						empregado[id][13] = "1.0";
 					} else {
-						empregado[id][5] = "0";
+						empregado[id][5] = "0.0";
 						empregado[id][6] = "mensalmente";
 						empregado[id][7] = "0";
 						empregado[id][8] = "30";
@@ -97,7 +98,7 @@ public class Main {
 
 				if (empregado[id][10].equals("Sim")) {
 					empregado[id][11] = Integer.toString(id);
-					System.out.println("Digite o valor $ da taxa sindical:");
+					System.out.println("Digite o valor $ da taxa sindical (Ex: 200.00):");
 					empregado[id][12] = sc.next();
 				}
 				System.out.println("O ID do empregado é: " + id + "\n");
@@ -132,32 +133,29 @@ public class Main {
 				
 				if (empregado[Id][2].equals("Horista")) {
 					System.out.println("Digite a hora de entrada:(Formato 24h)");
-					int HoradeEntrada = sc.nextInt();
+					double HoradeEntrada = sc.nextDouble();
 					System.out.println("Digite o minuto de entrada:");
-					int MinutodeEntrada = sc.nextInt();
+					double MinutodeEntrada = sc.nextDouble();
 					System.out.println("Digite a hora de saída:(Formato 24h)");
-					int HoradeSaida = sc.nextInt();
+					double HoradeSaida = sc.nextDouble();
 					System.out.println("Digite o minuto de saída:");
-					int MinutodeSaida = sc.nextInt();
+					double MinutodeSaida = sc.nextDouble();
 
-					int TempoemMinutos = (HoradeSaida * 60 + MinutodeSaida) - (HoradeEntrada * 60 + MinutodeEntrada);
-					int TempoExtra = TempoemMinutos - 480;
+					double TempoemMinutos = (HoradeSaida * 60.0 + MinutodeSaida) - (HoradeEntrada * 60.0 + MinutodeEntrada);
+					double TempoExtra = TempoemMinutos - 480.0;
 					double result = Double.parseDouble(empregado[Id][3]);
 					double resultSalario = Double.parseDouble(empregado[Id][14]);
-					double diaria = 0;
-					int diarias = Integer.parseInt(empregado[Id][13]);
+					double diaria = 0.0;
 
-					if (TempoExtra > 0) {
+					if (TempoExtra > 0.0) {
 						TempoemMinutos -= TempoExtra;
-						diaria += TempoExtra * 1.5 * result / 60;
+						diaria += TempoExtra * 1.5 * result / 60.0;
 					}
 
-					diaria += TempoemMinutos * result / 60;
+					diaria += TempoemMinutos * result / 60.0;
 					resultSalario += diaria;
-					diarias += 1;
 
 					empregado[Id][14] = Double.toString(resultSalario);
-					empregado[Id][13] = Integer.toString(diarias);
 				} else {
 					System.out.println("O empregado não é Horista.");
 				}
@@ -166,11 +164,10 @@ public class Main {
 
 				System.out.println("Digite o id do empregado que deseja lançar o resultado de venda:");
 				id = sc.nextInt();
-				sc.nextLine();
 				if (empregado[id][4].equals("Sim")) {
 					System.out.println("Digite o valor da venda:");
 					double res = sc.nextDouble();
-					double res2 = Double.parseDouble(empregado[id][5]);
+					double res2 = Double.parseDouble(empregado[id][13]);
 					res = res * res2 / 100;
 					double resSalario = Double.parseDouble(empregado[id][14]);
 
@@ -226,12 +223,14 @@ public class Main {
 						}
 					} else {
 						empregado[id][2] = "Horista";
-						empregado[id][7] = "1";
-						empregado[id][6] = "sexta";
 						System.out.println("Digite o valor da hora trabalhada:");
-						empregado[id][14] = sc.next();
+						empregado[id][3] = sc.next();
 						empregado[id][4] = "Nao";
 						empregado[id][5] = "0";
+						empregado[id][6] = "sexta";
+						empregado[id][7] = "1";
+						empregado[id][13] = "0";
+						empregado[id][14] = "0.0";
 					}
 				}
 				System.out.println("Deseja mudar o método de pagamento do empregado?(Sim/Nao)");
@@ -261,76 +260,80 @@ public class Main {
 					}
 				}
 			} else if (i == 7) {
-
+				
+				int k = 0;
 				String[] semana = { "domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sabado" };
 				String[] meses = { "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto",
 						"Setembro", "Outubro", "Novembro", "Dezembro" };
 				String diadaSemana = semana[(diaMes + 2 * mes) % 7];
 				System.out.println("Data: " + diaMes + "/" + meses[mes] + "   Dia da Semana: " + diadaSemana);
 
-				for (i = 0; i < 100; i++) {
-					if (empregado[i][4].equals("Sim")) {
-						double n = Double.parseDouble(empregado[i][14]);
-						n += n / 30;
-						empregado[i][14] = Double.toString(n);
-
-						if (diadaSemana.equals("domingo")) {
-							semanaQ += 1;
-							if (semanaQ > 4) {
-								semanaQ -= 4;
-							}
+				for (k = 0; k < 50; k++) {
+					
+					if (semanaQ % 2 == 0 && diadaSemana.equals(empregado[k][8]) && empregado[k][4].equals("Sim")) {
+						double salarioP = Double.parseDouble(empregado[k][14]);
+						double taxaDeVenda = Double.parseDouble(empregado[k][13]);
+						empregado[k][13] = "1.0";
+						double despesas = 0;
+						if (empregado[k][10].equals("Sim")) {
+							despesas += Double.parseDouble(empregado[k][12]);
 						}
-						if (semanaQ % 2 == 0 && diadaSemana.equals(empregado[i][8])) {
-							double salarioP = Double.parseDouble(empregado[i][14]);
-							empregado[i][14] = "0";
-							double despesas = 0;
-							if (empregado[i][10].equals("Sim")) {
-								despesas += Double.parseDouble(empregado[i][12]);
-							}
-							salarioP -= despesas;
-							System.out.println("Salário do empregado " + empregado[i][0] + ": R$" + salarioP + "\n");
-						}
-					} else if (empregado[i][2].equals("Assalariado")) {
-						double n = Double.parseDouble(empregado[i][14]);
-						n += n / 30;
-						int diaReceber = Integer.parseInt(empregado[i][8]);
+						salarioP -= despesas;
+						salarioP += taxaDeVenda;
+						System.out.println("Salário do empregado " + empregado[k][0] + ": R$" + salarioP + "\n");
+					}
+					
+					if ("Assalariado".equals(empregado[k][2]) && "Nao".equals(empregado[k][4])) {
+						
+						int diaReceber = Integer.parseInt(empregado[k][8]);
+						
 						if (diadaSemana.equals("sexta") && diaMes + 1 == diaReceber
 								|| diadaSemana.equals("sexta") && diaMes + 2 == diaReceber) {
-							double salarioP = Double.parseDouble(empregado[i][14]);
-							empregado[i][14] = "0";
-							double despesas = 0;
+							
+							double salarioP = Double.parseDouble(empregado[k][14]);
+							double despesas = 0.0;
 
-							if (empregado[i][10].equals("Sim")) {
-								despesas += Double.parseDouble(empregado[i][12]);
+							if (empregado[k][10].equals("Sim")) {
+								despesas += Double.parseDouble(empregado[k][12]);
 							}
 							salarioP -= despesas;
-							System.out.println("Salário do empregado " + empregado[i][0] + ": R$" + salarioP + "\n");
-						} else if (diaMes == Double.parseDouble(empregado[i][8]) && !diadaSemana.equals("sabado")
-								&& !diadaSemana.equals("domingo")) {
-							double salarioP = Double.parseDouble(empregado[i][14]);
-							empregado[i][14] = "0";
-							double despesas = 0;
+							System.out.println("Salário do empregado " + empregado[k][0] + ": R$" + salarioP + "\n");
+						}else if (diaMes == diaReceber) {
+							if(diadaSemana.equals("segunda") || diadaSemana.equals("terça") || diadaSemana.equals("quarta") || diadaSemana.equals("quinta") || diadaSemana.equals("sexta")) {
+								double salarioP = Double.parseDouble(empregado[k][14]);
+								double despesas = 0.0;
 
-							if (empregado[i][10].equals("Sim")) {
-								despesas += Double.parseDouble(empregado[i][12]);
+								if (empregado[k][10].equals("Sim")) {
+									despesas += Double.parseDouble(empregado[k][12]);
+								}
+								salarioP -= despesas;
+								System.out.println("Salário do empregado " + empregado[k][0] + ": R$" + salarioP + "\n");
 							}
-							salarioP -= despesas;
-							System.out.println("Salário do empregado " + empregado[i][0] + ": R$" + salarioP + "\n");
-						}
-					} else if (empregado[i][2].equals("Horista")) {
-						if (diadaSemana.equals(empregado[i][6])) {
-							double salarioP = Double.parseDouble(empregado[i][14]);
-							empregado[i][14] = "0";
-							double despesas = 0;
-							if (empregado[i][10].equals("Sim")) {
-								despesas += Double.parseDouble(empregado[i][12]);
-							}
-							salarioP -= despesas;
-							System.out.println("Salário do empregado " + empregado[i][0] + ": R$" + salarioP + "\n");
 						}
 					}
+					
+					if ("Horista".equals(empregado[k][2])) {
+						if (diadaSemana.equals(empregado[k][8])) {
+							double salarioP = Double.parseDouble(empregado[k][14]);
+							empregado[k][14] = "0.0";
+							double despesas = 0.0;
+							if (empregado[k][10].equals("Sim")) {
+								despesas += Double.parseDouble(empregado[k][12]);
+							}
+							salarioP -= despesas;
+							System.out.println("Salário do empregado " + empregado[k][0] + ": R$" + salarioP + "\n");
+						}
+					}
+					
 				}
+			 if (diadaSemana.equals("domingo")) {
+				 semanaQ += 1;
+				if (semanaQ > 4) {
+					semanaQ -= 3;
+				}
+			 }
 				diaMes += 1;
+				System.out.println("Encerrou mais um dia de trabalho");
 
 			} else if (i == 8) {
 
@@ -389,7 +392,7 @@ public class Main {
 			} else if (i == 11) {
 
 				int k = 0;
-				for (k = 0; k < 100; k++) {
+				for (k = 0; k < 50; k++) {
 					if (idLivres[k] != 0) {
 						System.out.printf("Nome: " + empregado[k][0] + " Endereco: " + empregado[k][1] + " Tipo: "
 								+ empregado[k][2] + " Valor de hora trabalhada: " + empregado[k][3] + " Comissionado: "
